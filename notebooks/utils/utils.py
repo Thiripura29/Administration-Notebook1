aacc2a22ebb856c99d1e2f30f881701d98462393
Thiripura29 <Thiripura29>
2024-09-24 05:23:27 +0000

    secretutils

---
+++ notebooks/utils/utils.py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+def get_jdbc_credentials_from_scope(scope_name):
+    try:
+        user_name=dbutils.secrets.get(scope=scope_name,key="user_name")
+        password=dbutils.secrets.get(scope=scope_name,key="password")
+        return {"user_name": user_name, "password": password}
+    except Exception as e:
+        raise e
+
+# COMMAND ----------
+
+import json
+import traceback
+def load_config(config_path):
+        """
+        Load configuration from a JSON file.
+
+        This method reads the JSON file specified by the `config_path` and
+        returns the parsed configuration as a dictionary.
+
+        :return: Parsed JSON configuration as a dictionary.
+        :raises ValueError: If the config file format is not JSON.
+        """
+        try:
+            with open(config_path, 'r') as f:
+                   if config_path.endswith('.json'):
+                    return json.load(f)
+                   else:
+                        raise ValueError("Unsupported config file format. Use JSON.")
+        except Exception as e:
+            print(traceback.format_exc())
