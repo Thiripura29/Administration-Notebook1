01a7daf0788d057eaf9a9535e5b57c8cba7fa11c
Thiripura29 <Thiripura29>
2024-09-24 13:24:27 +0000

    Full-load-Organization

--- notebooks/Bronze-jdbc-template-full-load.py
+++ notebooks/Bronze-jdbc-template-full-load.py
@@ -1,9 +1,9 @@
 # Databricks notebook source
-# MAGIC %run ./utils/util
+# MAGIC %run ./utils/utils
 
 # COMMAND ----------
 
-dbutils.widgets.text("config_path","/Workspace/Users/chinnasamythiripura@gmail.com/Administration-Notebook1/configs")
+dbutils.widgets.text("config_path","../configs/dev1.json")
 
 # COMMAND ----------
 
@@ -14,50 +14,85 @@ config=load_config(config_path)
 
 # COMMAND ----------
 
+print(config)
+
+# COMMAND ----------
+
 print(config_path)
 
 # COMMAND ----------
 
-import json
-import traceback
-
-config_path=dbutils.widgets.get("config_path")
-config_data = None
-with open(config_path, 'r') as f:
-    if config_path.endswith('.json'):
-        config_data= json.load(f)
-    else:
-        raise ValueError("Unsupported config file format. Use JSON.")
-    display(config_data)
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
 database_config=config["database-config"]
-health_config=config["health-config"]
+health_config=database_config["healthcare"]
 
 
 # COMMAND ----------
 
 url=health_config["url"]
 jdbc_credentials=get_jdbc_credentials_from_scope(health_config["scope_name"])
-username=jdbc_credentials["username"]
+username=jdbc_credentials["user_name"]
 password=jdbc_credentials["password"]
-driver_name=health_config["driver_name"]
+driver_name=health_config["driver"]
 
 # COMMAND ----------
 
-sources=health_config["sources"]
-for source in sources:
-    if source["load_type"]=="full_load":
-spark.read.format("jdbc").options(
-    {
-        "url": url,
-        "dbtable": "health_data"
-        "user": username
-        "password": password
-    }
-)
+options= {
+"url":f"{url}/healthcare",
+"dbtable":"organizations",
+"user":username,
+"password":password
+}
+df=spark.read.format("jdbc").options(**options).load()
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+from pyspark.sql.functions import day, month, year, lit,col
+columns=df.columns
+transform_df=df\
+                .withColumn("audit_source_record_id", F.hash(F.concat_ws("||", *columns)))\
+                .withColumn("audit_ingestion_timestamp", F.current_timestamp())
+
+transform_df=transform_df\
+               .withColumn("Day",day(col("audit_ingestion_timestamp")))\
+               .withColumn("Month",month(col("audit_ingestion_timestamp")))\
+               .withColumn("Year",year(col("audit_ingestion_timestamp")))\
+               .withColumn("department",lit("administration"))
+display(transform_df)
+
+
+# COMMAND ----------
+
+spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
+spark.conf.set("spark.databricks.delta.optimizeWrite.enabled", True)
+spark.conf.set("spark.databricks.delta.autoCompact.enabled", True)
+spark.conf.set("spark.databricks.delta.autoOptimize.optimizeWrite", True)
+spark.conf.set("spark.databricks.delta.schema.autoMerge.enabled", True)
+
+# COMMAND ----------
+
+db_bucket=config["bucket-name"]
+bronze_prefix=config["bronze-prefix"]
+table_name="organizations"
+catalog=config["catalog-name"]
+schema_name=config["schema-name"]
+
+target_path=f"s3://{db_bucket}/{bronze_prefix}/{table_name}"
+target_table_path=f"{catalog}.{schema_name}.bronze_{table_name}"
+
+transform_df\
+    .write\
+    .format("delta")\
+    .mode("overwrite")\
+    .partitionBy("year","month","day")\
+    .option("path",target_path)\
+    .saveAsTable(target_table_path)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from lakehouse_dev.administration.bronze_organizations
--- notebooks/utils/utils.py
+++ notebooks/utils/utils.py
@@ -1,31 +1,31 @@
 # Databricks notebook source
 def get_jdbc_credentials_from_scope(scope_name):
     try:
-        user_name=dbutils.secrets.get(scope=scope_name,key="user_name")
-        password=dbutils.secrets.get(scope=scope_name,key="password")
+        user_name=dbutils.secrets.get(scope=scope_name,key="db_username")
+        password=dbutils.secrets.get(scope=scope_name,key="db_password")
         return {"user_name": user_name, "password": password}
     except Exception as e:
         raise e
 
 # COMMAND ----------
 
-import json
 import traceback
+import json
 def load_config(config_path):
-        """
-        Load configuration from a JSON file.
+    """
+    Load configuration from a JSON file.
 
-        This method reads the JSON file specified by the `config_path` and
-        returns the parsed configuration as a dictionary.
+    This method reads the JSON file specified by the `config_path` and
+    returns the parsed configuration as a dictionary.
 
-        :return: Parsed JSON configuration as a dictionary.
-        :raises ValueError: If the config file format is not JSON.
-        """
-        try:
-            with open(config_path, 'r') as f:
-                   if config_path.endswith('.json'):
-                    return json.load(f)
-                   else:
-                        raise ValueError("Unsupported config file format. Use JSON.")
-        except Exception as e:
-            print(traceback.format_exc())
+    :return: Parsed JSON configuration as a dictionary.
+    :raises ValueError: If the config file format is not JSON.
+    """
+    try:
+        with open(config_path, 'r') as f:
+            if config_path.endswith('.json'):
+                return json.load(f)
+            else:
+                    raise ValueError("Unsupported config file format. Use JSON.")
+    except Exception as e:
+        print(traceback.format_exc())
